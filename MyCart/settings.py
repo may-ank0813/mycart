a5bce39f606cd01cf4eb253f3b148d2e2899882b
Mayank Saini <iec2019009@iiita.ac.in>
2021-03-30 12:34:49 +0530

    changing procfile

--- MyCart/settings.py
+++ MyCart/settings.py
@@ -16,8 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +25,7 @@ SECRET_KEY = 'cfvgbhnjkm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','my-cart1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'my-cart1.herokuapp.com']
 
 
 # Application definition
@@ -41,13 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     # 'shop',
-     # 'blog'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,7 +57,7 @@ ROOT_URLCONF = 'MyCart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': ['MyCart/templates'],
+        'DIRS': ['MyCart/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,15 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL= "/static/"
+STATIC_URL = "/static/"
 
 # managing media
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-MEDIA_URL="/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 django_heroku.settings(locals())
-
-
-
-
--- MyCart/urls.py
+++ MyCart/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include("blog.urls")),
 
-url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
